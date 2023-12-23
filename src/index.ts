152d1e233be8a66397ddd2323a7f8e6a23e0b152
alby <100741622+albywok@users.noreply.github.com>
2023-12-23 18:49:23 +0000

    useCmv3, version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Cmv3Provider } from './context/Cmv3Provider'
-import { Cmv3Context } from './context/Cmv3Context'
+import { useCmv3 } from './context/Cmv3Context'
 import Clock from './components/Clock'
 
-export { Cmv3Provider, Cmv3Context, Clock }
+export { Cmv3Provider, useCmv3, Clock }
