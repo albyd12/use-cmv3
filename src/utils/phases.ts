da5daad4d7425b110d8e14f2e43f38cd13936d20
0xalby <alby.dev@icloud.com>
2024-01-02 10:45:18 +0000

    add freezeSolPayment

--- src/utils/phases.ts
+++ src/utils/phases.ts
@@ -3,6 +3,8 @@ import { Umi } from '@metaplex-foundation/umi'
 import { allowlistChecker, mintLimitChecker } from './checker'
 import { AllowLists } from '../types/metadata'
 import { Payment, Phase } from '../types/phase'
+import { fetchMint, fetchToken } from '@metaplex-foundation/mpl-toolbox'
+import { fetchMetadata } from '@metaplex-foundation/mpl-token-metadata'
 
 export const refreshPhaseTime = (phase: Phase): Phase => {
   const refreshdPhase: Phase = { ...phase }
@@ -41,7 +43,7 @@ export const getMintPhases = async (
       let endDate: number | undefined = undefined
       let startDate: number | undefined = undefined
 
-      //address based guards
+      /* ADDRESS GUARDS */
       if (group.guards.allowList.__option == 'Some') {
         if (!allowLists || !allowlistChecker(allowLists, umi, group.label)) {
           errors.push('Address not allowed in phase')
@@ -55,14 +57,13 @@ export const getMintPhases = async (
         }
       }
 
-      //time based guards - these are static defaults which will be overrrdidden further down.
+      /* TIME GUARDS */
       if (group.guards.startDate.__option === 'Some') {
         startDate = Number(group.guards.startDate.value.date) * 1000
         if (Date.now() < startDate) {
           errors.push('Phase not commenced')
         }
       }
-
       if (group.guards.endDate.__option === 'Some') {
         endDate = Number(group.guards.endDate.value.date) * 1000
         if (Date.now() > endDate) {
@@ -70,7 +71,9 @@ export const getMintPhases = async (
         }
       }
 
-      //payments
+      /* PAYMENTS*/
+
+      /* SOL */
       if (group.guards.solPayment.__option === 'Some') {
         const payment = group.guards.solPayment.value.lamports
         payments.push({
@@ -82,6 +85,18 @@ export const getMintPhases = async (
           errors.push('Not enough funds')
         }
       }
+      if (group.guards.freezeSolPayment.__option === 'Some') {
+        const payment = group.guards.freezeSolPayment.value.lamports
+        payments.push({
+          identifier: payment.identifier,
+          decimals: Number(payment.decimals),
+          basisPoints: Number(payment.basisPoints),
+        })
+        if (Number(solBalance.basisPoints) < Number(payment.basisPoints)) {
+          errors.push('Not enough funds')
+        }
+      }
+
 
       return {
         label: group.label,
